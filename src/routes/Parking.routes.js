9b2b31c01a3893cdb842f7a3d1e9658918e40a94
Clever <1510980@senati.pe>
2025-04-24 11:30:37 -0500

    se Actualizo la parte de Tarifa

--- src/routes/Parking.routes.js
+++ src/routes/Parking.routes.js
@@ -2,5 +2,17 @@ import { Router } from "express";
 const rutas = Router();
 
 //APIS
+import { obtenerTarifario, ObtenerTarifaId, InsertarTarifa, deleteTarifa } from "../controllers/TarifaController.js";
+//api para mostrar el tarfario de parqueo
+rutas.get('/Tarifario', obtenerTarifario);
+
+//Api para mostrar el tarifario por id
+rutas.get('/TarifarioID/:IdTarifa', ObtenerTarifaId);
+
+//Api para insertar nuevo Tarifario
+rutas.post('/InsertarTarifario', InsertarTarifa);
+
+//Api para Eliminar Tarifario
+rutas.delete('/EliminarTarifario/:IdTarifa', deleteTarifa);
 
 export default rutas;
